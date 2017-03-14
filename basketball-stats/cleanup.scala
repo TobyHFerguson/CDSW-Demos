ab5b48cda7a3781a4e9c26ffe590af7891bf9845
jordanvolz <jordan.volz@cloudera.com>
2017-03-14 18:40:46 -0400

    Create cleanup.scala

---
+++ basketball-stats/cleanup.scala
@@ -0,0 +1,7 @@
+spark.sql("DROP TABLE IF EXISTS basketball.players")
+spark.sql("DROP TABLE IF EXISTS basketball.age")
+spark.sql("DROP TABLE IF EXISTS basketball.experience")
+spark.sql("DROP TABLE IF EXISTS basketball.similar")
+spark.sql("DROP TABLE IF EXISTS basketball.ml")
+spark.sql("DROP TABLE IF EXISTS basketball.regression_total")
+
