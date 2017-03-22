cb56f8d3e6a766a03f66e7dcaa740ee5beec570c
jordanvolz <jordan.volz@cloudera.com>
2017-03-22 14:42:41 -0400

    move out setup code

--- hail-genetics-tutorial/tutorial.py
+++ hail-genetics-tutorial/tutorial.py
@@ -4,33 +4,6 @@
 # 
 # In this tutorial, we will analyze data from the final phase of the [1000 Genomes Project](http://www.internationalgenome.org/about), as described in [A global reference for human genetic variation (Nature 2015)](http://www.nature.com/nature/journal/v526/n7571/full/nature15393.html). We have downsampled the dataset to approximately 10,000 variants consisting of both rare and common variants. We obtained sample information (population, super-population, sex) from the [1000 Genomes website](http://www.internationalgenome.org/data-portal/sample).
 # 
-# ## Download tutorial data files
-# 
-# Download the zip file *Hail_Tutorial_Data-v2.tgz* using [wget](https://www.google.com/search?q=install+wget) or [curl](https://www.google.com/search?q=install+curl):
- 
-! wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz
-
-# Unzip the file:
-# 
-
-! tar -xvzf Hail_Tutorial_Data-v2.tgz
-
-# Copy to HDFS:
-# 
-
-! hadoop fs -put Hail_Tutorial-v2 .
-
-# 
-# The contents are as follows:
-#   
-#   - 1000 Genomes compressed VCF (downsampled to 10K variants, 248 samples):
-#     - *1000Genomes_248samples_coreExome10K.vcf.bgz*
-#   - Sample annotations:
-#     - *1000Genomes.sample_annotations*
-#   - LD-pruned SNP list: 
-#     - *purcell5k.interval_list*
-# 
-#
 # In the interactive workbench session, type these commands:
 # 
 # 
