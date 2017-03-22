7c64dca8eebc9d3b85c715b5652d081cee3da675
jordanvolz <jordan.volz@cloudera.com>
2017-03-22 14:43:11 -0400

    Create setup.sh

---
+++ hail-genetics-tutorial/setup.sh
@@ -0,0 +1,27 @@
+# ## Download tutorial data files
+# 
+# Download the zip file *Hail_Tutorial_Data-v2.tgz* using [wget](https://www.google.com/search?q=install+wget) or [curl](https://www.google.com/search?q=install+curl):
+ 
+wget https://storage.googleapis.com/hail-tutorial/Hail_Tutorial_Data-v2.tgz
+
+# Unzip the file:
+# 
+
+tar -xvzf Hail_Tutorial_Data-v2.tgz
+
+# Copy to HDFS:
+# 
+
+hadoop fs -put Hail_Tutorial-v2 .
+
+# 
+# The contents are as follows:
+#   
+#   - 1000 Genomes compressed VCF (downsampled to 10K variants, 248 samples):
+#     - *1000Genomes_248samples_coreExome10K.vcf.bgz*
+#   - Sample annotations:
+#     - *1000Genomes.sample_annotations*
+#   - LD-pruned SNP list: 
+#     - *purcell5k.interval_list*
+# 
+#
