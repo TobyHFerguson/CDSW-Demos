9cb6e79ecf70d287e3220c504e61ab048cb2d833
jordanvolz <jordan.volz@cloudera.com>
2017-03-22 14:44:40 -0400

    Update cleanup.sh

--- hail-genetics-tutorial/cleanup.sh
+++ hail-genetics-tutorial/cleanup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -rf Hail_Tutorial-v2
-rm -f Hail_Tutorial_Data-v2.tgz
+rm -rf hail-genetics-tutorial/Hail_Tutorial-v2
+rm -f hail-genetics-tutorial/Hail_Tutorial_Data-v2.tgz
 hadoop fs -rm -r Hail_Tutorial-v2
 hadoop fs -rm -r 1kg.vds
