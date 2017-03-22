ee447d3d4dbffe0497185529e7243b0e7688b2a2
jordanvolz <jordan.volz@cloudera.com>
2017-03-22 13:12:50 -0400

    Update and rename flights.r to flight-analytics.R

--- flight-analytics/flight-analytics.R
+++ flight-analytics/flight-analytics.R
@@ -1,18 +1,16 @@
 ## Load libraries
 library(ggplot2)
-install.packages("maps")
 library(maps)
 library(geosphere)
 library (DBI)
 
 ## Loading required package: sparklyr + dplyr
-install.packages("sparklyr")
 library(sparklyr)
 library(dplyr)
 
 ## Configure cluster
 config <- spark_config()
-config$spark.driver.cores   <- 4
+config$spark.driver.cores   <- 2
 config$spark.executor.cores <- 4
 config$spark.executor.memory <- "4G"
 
@@ -95,29 +93,3 @@ for (j in 1:length(flights_aa$carrier)) {
   lines(inter, col=colors[colindex], lwd=0.8)
 }
 
-
-#We will build a predictive model with MLlib. We use linear regression of MLlib.
-
-#First, we will prepare training data. In order to handle categorical data, you should use tf_string_indexer for converting.
-
-# build predictive model with linear regression
-partitions <- airlines %>%
-  filter(arrdelay >= 5) %>%
-  sdf_mutate(
-       carrier_cat = ft_string_indexer(carrier),
-       origin_cat = ft_string_indexer(origin),
-       dest_cat = ft_string_indexer(dest)
-  ) %>%
-  mutate(hour = floor(dep_time/100)) %>%
-  sdf_partition(training = 0.5, test = 0.5, seed = 1099)
-fit <- partitions$training %>%
-   ml_linear_regression(
-     response = "arrdelay",
-     features = c(
-        "month", "hour", "dayofweek", "carrier_cat", "depdelay", "origin_cat", "dest_cat", "distance"
-       )
-    )
-
-fit
-
-summary(fit)
