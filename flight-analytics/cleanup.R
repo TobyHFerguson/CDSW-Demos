73ab821338fbaac785e01493a32793114adcc22b
jordanvolz <jordan.volz@cloudera.com>
2017-03-23 22:55:17 -0400

    Update cleanup.R

--- flight-analytics/cleanup.R
+++ flight-analytics/cleanup.R
@@ -1,4 +1,5 @@
 library (DBI)
+library (sparklyr)
 
 ## Configure cluster
 config <- spark_config()
@@ -10,6 +11,7 @@ spark_home <- "/opt/cloudera/parcels/SPARK2/lib/spark2"
 spark_version <- "2.0.0"
 sc <- spark_connect(master="yarn-client", version=spark_version, config=config, spark_home=spark_home)
 
+tbl_change_db(sc,"flight")
 dbSendQuery(sc, "DROP TABLE airports_str")
 dbSendQuery(sc, "DROP TABLE airports")
 dbSendQuery(sc, "DROP TABLE airlines_bi_pq")
