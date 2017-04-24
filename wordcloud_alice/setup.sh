0b4da688bf3f1886acebd21e34f6ce07289a4f1d
jordanvolz <jordan.volz@cloudera.com>
2017-04-24 13:26:21 -0400

    Update setup.sh

--- wordcloud_alice/setup.sh
+++ wordcloud_alice/setup.sh
@@ -3,3 +3,5 @@
 wget http://www.umich.edu/~umfandsf/other/ebooks/alice30.txt -P /tmp
 hdfs dfs -put /tmp/alice30.txt /tmp/
 wget "http://www.stencilry.org/stencils/movies/alice%20in%20wonderland/255fk.jpg?p=*full-image" -O wordcloud_alice/resources/alice-mask.jpg
+
+pip install -r wordcloud_alice/requirements.txt
